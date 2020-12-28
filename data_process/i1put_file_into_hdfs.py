8f587d8f512a52a51dc5c602f7ad460b024b357a
greatabel <myreceiver2for2github@gmail.com>
2020-12-28 14:42:28 +0800

    i1put_file_into_hdfs.py

---
+++ data_process/i1put_file_into_hdfs.py
@@ -0,0 +1,22 @@
+'''
+# 以 100w条数据的 2020-12-28_generated_demo.csv 为例子，步骤如下：
+
+# 关闭只读模式 
+hdfs dfsadmin -safemode leave
+
+# 删除旧的同名数据
+hdfs dfs -rm -r 2020-12-28_generated_demo.csv
+
+# 在hdfs上创建data目录
+hdfs dfs -mkdir data
+
+# 上传100w记录的csv
+hdfs dfs -copyFromLocal 2020-12-28_generated_demo.csv data
+
+#查看文件
+hdfs dfs -ls data
+
+
+
+
+'''

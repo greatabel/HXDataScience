934951387943c0a837cc5fd36290960999de5d0d
greatabel <myreceiver2for2github@gmail.com>
2021-05-02 07:42:29 +0800

    join spark

--- data_process/i3spark_anlysis.py
+++ data_process/i3spark_anlysis.py
@@ -1,4 +1,5 @@
 import findspark
+
 findspark.init()
 # A simple demo for working with SparkSQL and Tweets
 from pyspark import SparkContext, SparkConf
@@ -12,32 +13,92 @@ from termcolor import colored
 # inputFile = '../original_data/smi_sensor_202011_202011261650.csv'
 
 # 没有表头
-inputFile = '../original_data/smi_sensor_all_*.csv'
+inputFile = "../original_data/smi_sensor_all_*.csv"
 conf = SparkConf().setAppName("SparkSQLSMI")
 sc = SparkContext()
 hiveCtx = HiveContext(sc)
 print("Loading data from " + inputFile)
 
-input = hiveCtx.read.csv(inputFile,
-        inferSchema="true",
-        quote="").toDF("id","company_id","facility_one",
-    "facility_two","facility_three","monitor_id","plant_id","cost_id","sensor_status",
-    "energy","service_time","total_time","sensor_type","sensor_value","create_by","create_time",
-    "update_by","update_time","remark","is_del","original_value","batch_time","packet","conversion_id",
-    "sensor_id","voltage","facility_type")
-'''
+input = hiveCtx.read.csv(inputFile, inferSchema="true", quote="").toDF(
+    "id",
+    "company_id",
+    "facility_one",
+    "facility_two",
+    "facility_three",
+    "monitor_id",
+    "plant_id",
+    "cost_id",
+    "sensor_status",
+    "energy",
+    "service_time",
+    "total_time",
+    "sensor_type",
+    "sensor_value",
+    "create_by",
+    "create_time",
+    "update_by",
+    "update_time",
+    "remark",
+    "is_del",
+    "original_value",
+    "batch_time",
+    "packet",
+    "conversion_id",
+    "sensor_id",
+    "voltage",
+    "facility_type",
+)
+"""
 ' select `id`, `company_id`, `facility_one`, 
 `facility_two`, `facility_three`, `monitor_id`, `plant_id`, `cost_id`, `sensor_status`,
  `energy`, `service_time`, `total_time`, `sensor_type`, `sensor_value`, `create_by`, `create_time`,
   `update_by`, `update_time`, `remark`, `is_del`, `original_value`, `batch_time`, `packet`, `conversion_id`,
    `sensor_id`, `voltage`, `facility_type`
-'''
+"""
+
 input.registerTempTable("smi_sensor")
 # print(input.show())
-topTweets = hiveCtx.sql("SELECT monitor_id, sensor_type, batch_time, sensor_value \
- FROM smi_sensor ORDER BY create_time asc LIMIT 10")
-print('依据 A =', topTweets.collect() )
+topSensors = hiveCtx.sql(
+    "SELECT monitor_id, sensor_type, batch_time, sensor_value \
+ FROM smi_sensor ORDER BY create_time asc LIMIT 50"
+)
+print("依据 A =", topSensors.collect())
 
+
+print("\n\n")
+
+
+inputFile_monitor = "../original_data/smi_monitor_luquan.csv"
+
+print("Loading data from " + inputFile_monitor)
+input1 = hiveCtx.read.csv(inputFile_monitor, inferSchema="true", quote="")
+input1.registerTempTable("smi_mointor")
+topMonitors = hiveCtx.sql(
+    "SELECT _c0 as monitor_id ,_c2 as monitor_code \
+ FROM smi_mointor "
+)
+
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import *
+
+# remove quotes “ ” from a column of a Spark dataframe in pyspark
+topMonitors = topMonitors.withColumn(
+    "monitor_id", regexp_replace("monitor_id", '"', "")
+)
+topMonitors = topMonitors.withColumn(
+    "monitor_code", regexp_replace("monitor_code", '"', "")
+)
+
+# 转换字符串为int
+topMonitors = topMonitors.withColumn(
+    "monitor_id", topMonitors["monitor_id"].cast(IntegerType())
+)
+
+print("依据 B =", topMonitors.collect())
+print(type(topMonitors))
+
+print("\n--join--\n")
+topSensors.join(topMonitors, ["monitor_id"], "left").show()
 # https://stackoverflow.com/questions/39535447/attributeerror-dataframe-object-has-no-attribute-map
 # topTweetText = topTweets.rdd.map(lambda row : row.text)
 # print(topTweetText.collect() )
